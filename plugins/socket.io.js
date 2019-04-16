b8a0a6cd7bc08c3b5fb2c5752afb4b98ede2d6e1
Menno <mennodevries1@msn.com>
2019-04-16 12:57:35 +0200

    add config

--- plugins/socket.io.js
+++ plugins/socket.io.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import io from 'socket.io-client';
 import VueSocketIO from 'vue-socket.io-extended';
+
+let config = require('../nuxt.config.js')
 config.dev = !(process.env.NODE_ENV === 'production')
 const { host, port } = nuxt.options.server
 
