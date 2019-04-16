ecb178fd64044cf4b71636e8715dff81b14ed8ad
Menno <mennodevries1@msn.com>
2019-04-16 13:04:46 +0200

    localhost

--- plugins/socket.io.js
+++ plugins/socket.io.js
@@ -2,11 +2,6 @@ import Vue from 'vue';
 import io from 'socket.io-client';
 import VueSocketIO from 'vue-socket.io-extended';
 
-let config = require('../nuxt.config.js')
-config.dev = !(process.env.NODE_ENV === 'production')
-const { host, port } = nuxt.options.server
-
-
 export default ({ store }) => {
-  Vue.use(VueSocketIO, io(`http://${host}:${port}`), { store });
+  Vue.use(VueSocketIO, io(`http://localhost:3000`), { store });
 }
