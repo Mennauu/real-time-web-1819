825fdd08f2652373d687dfbde19eda793c4b7d79
Menno <mennodevries1@msn.com>
2019-04-16 12:55:07 +0200

    host change

--- plugins/socket.io.js
+++ plugins/socket.io.js
@@ -1,7 +1,10 @@
 import Vue from 'vue';
 import io from 'socket.io-client';
 import VueSocketIO from 'vue-socket.io-extended';
+config.dev = !(process.env.NODE_ENV === 'production')
+const { host, port } = nuxt.options.server
+
 
 export default ({ store }) => {
-  Vue.use(VueSocketIO, io(), { store });
+  Vue.use(VueSocketIO, io(`http://${host}:${port}`), { store });
 }
