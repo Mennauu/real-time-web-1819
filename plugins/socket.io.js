d3ac2a0bd772c8b28b1d54a2490c12cbca3b5af5
Menno <mennodevries1@msn.com>
2019-04-16 11:57:13 +0200

    Port change

--- plugins/socket.io.js
+++ plugins/socket.io.js
@@ -3,6 +3,5 @@ import io from 'socket.io-client';
 import VueSocketIO from 'vue-socket.io-extended';
 
 export default ({ store }) => {
-  const port = process.env.PORT || 3000
-  Vue.use(VueSocketIO, io(`http://localhost:${port}`), { store });
+  Vue.use(VueSocketIO, io(`http://localhost:${process.env.PORT}`), { store });
 }
